47642327111768d2c57d61510c9ee8b699f86255
caty <1120873075@qq.com>
2021-03-30 12:04:01 +0800

    123

--- public/js/src/scroll-cookie.js
+++
@@ -1,3 +0,0 @@
-// build time:Wed Mar 11 2020 22:11:33 GMT+0800 (香港标准时间)
-$(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
-//rebuild by neat 
